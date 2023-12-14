589c19adba338e616c040d30baba00d1172845bf
Courtney Caputo <courtneycaputo@Courtneys-MacBook-Air.local>
2023-12-13 16:07:55 -0800

    update

--- src/components/Pages/Biography.js
+++ src/components/Pages/Biography.js
@@ -24,15 +24,17 @@ export default function Biography() {
       
     <section id="biography" className="section" >
     <Row>
+    <Col sm={1}></Col>
     <Col sm={4}><img src={Courtney} className="my-picture" alt="Courtney" /></Col>
-    <Col sm={6}>
+    <Col sm={5}>
       <h2>About</h2>
+      <div className="biofont">
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
   <br></br>
        <br></br>
        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-
-       <h2><Link  to="contact">Contact Bethany today!</Link></h2>
+       </div>
+       
        </Col>
        </Row>
     </section>
